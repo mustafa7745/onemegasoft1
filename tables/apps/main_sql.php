cba19ea41e61117cb9a9a0dcbb4dc804b672656f
Mustafa Ismail <mustafa774519161@gmail.com>
2024-01-31 04:01:07 -0800

    add path url

--- tables/apps/main_sql.php
+++ tables/apps/main_sql.php
@@ -11,8 +11,11 @@ class MainSqlApps extends AppsAttribute
     }
     function r_sql($innerJoin, $condition): string
     {
+        require_once($_SERVER["DOCUMENT_ROOT"] . '/onemegasoft1/tables/static/anonymous/sql.php');
+        $anonymous_static_sql = new Anonymous_StaticSql();
+        // 
         $table_name = $this->table_name;
-        $column = " * ";
+        $column = " * "." , " . $anonymous_static_sql->read_path_icon_app_sql("'path_icon_app'");
         /////
         return read_by_condition_sql($table_name, $column, $innerJoin, $condition);
     }
